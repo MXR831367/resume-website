354ecbeccc1feb4e6fd768759ed17772ead818e2
Matthew Roeder <mroeder@hellyes.net>
2024-10-19 15:16:18 -0400

    Reorganize

---
+++ frontend/main.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
+});
+
+const functionApi = "";
+
+const getVisitCount = () => {
+  let count = 37;
+  fetch(functionApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("counter").inntrText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
