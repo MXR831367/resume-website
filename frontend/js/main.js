3dc1737866ab5d272a6add0bfd6cdc887c7b3d1c
MXR831367 <mroeder@hellyes.net>
2025-08-29 10:47:39 -0400

    javascript cleanup

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,39 +1,44 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  getVisitCount();
-});
+// Function to set the theme color based on the CSS variable
+const setThemeColor = () => {
+    // Get the computed value of the CSS variable
+    const themeColor = getComputedStyle(document.documentElement)
+        .getPropertyValue("--primary-color")
+        .trim();
 
-const functionApi =
-  "https://mxr-website-counter.azurewebsites.net/api/count?id=1";
-
-const getVisitCount = () => {
-  let count = 37;
-  console.log("getVisitCount started.");
-  fetch(functionApi)
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      console.log("Website called function API.");
-      count = response.count;
-      document.getElementsByClassName(
-        "countertext"
-      )[0].innerText = `VIEWS: ${count}`;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  return count;
+    // Find the meta tag and update its content
+    const metaTag = document.getElementById("theme-color-meta");
+    if (metaTag) {
+        metaTag.setAttribute("content", themeColor);
+    }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Get the computed value of the CSS variable
-  const themeColor = getComputedStyle(document.documentElement)
-    .getPropertyValue("--primary-color")
-    .trim();
+const functionApi =
+    "https://mxr-website-counter.azurewebsites.net/api/count?id=1";
 
-  // Find the meta tag and update its content
-  const metaTag = document.getElementById("theme-color-meta");
-  if (metaTag) {
-    metaTag.setAttribute("content", themeColor);
-  }
+const getVisitCount = () => {
+    let count = 37;
+    console.log("getVisitCount started.");
+    fetch(functionApi)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementsByClassName(
+                "countertext"
+            )[0].innerText = `VIEWS: ${count}`;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    return count;
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Call the function to set the theme color
+    setThemeColor();
+
+    // Call your existing function to get the visit count
+    getVisitCount();
 });
